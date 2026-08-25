7ad531fd2b43d51076c009ee482bf5c4b3ff5c9a
serhioses <serega.mirosh@gmail.com>
2026-08-25 14:03:30 +0000

    Add an AI client and print token usage

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
 def main():
-    print("Hello from aiagent!")
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+
+    if api_key is None:
+        raise RuntimeError("OPENROUTER_API_KEY is not set.")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=[
+            {
+                "role": "user",
+                "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+            }
+        ],
+    )
+    if response.usage is None:
+        raise RuntimeError("Failed to get usage object on response.")
+    print(f"Prompt tokens: {response.usage.prompt_tokens}")
+    print(f"Response tokens: {response.usage.completion_tokens}")
+    answer = response.choices[0].message.content
+
+    print(f"Response:\n {answer}")
 
 
 if __name__ == "__main__":
